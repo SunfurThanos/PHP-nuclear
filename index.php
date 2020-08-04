f5eb4c585e0c453e44d83c5e29c8335bc7d52add
SunfurThanos <hormigence123@gmail.com>
2020-08-04 11:34:14 -0430

    Sunfur

--- index.php
+++ index.php
@@ -5,7 +5,6 @@
 <link href="images/consola.png" rel="icon" type="image/x-icon">
 <title>Heroku poverfull</title>
 
-
 <link rel="stylesheet" href="spectre.min.css">
 <link rel="stylesheet" href="spectre-exp.min.css">
 <link rel="stylesheet" href="spectre-icons.min.css">
@@ -13,7 +12,7 @@
 
 <div align="center">
     <br>
-	<h3>MADE in PHP, Python, Spectre & Heroku</h2>
+	<h3>Made in PHP, Python, Spectre & Heroku</h2>
 	<img src="images/image05.jpeg" width="150">
 	<br><br>
 	<textarea placeholder="escribe texto a imprimir" style="width: 300px;height: 200px;padding-left: 0.4em;" id="IDE"></textarea>
@@ -36,12 +35,13 @@ function httpRequest(url, callback) {
     http.open('GET', url);
     http.send();
 
-    http.onreadystatechange = function(){
+    http.onreadystatechange = function() {
         if(this.readyState == 4 && this.status == 200) {
             callback.apply(http);
         }
     }
 }
+
 
 function SEND_server_java() {
 
@@ -49,9 +49,12 @@ function SEND_server_java() {
         return editor.focus()
     }
 
+    valor = editor.value
+    debug.value = ""
     boton.className = "btn loading"
+    editor.value = ""
 
-    httpRequest('morfeo.php?text-IDE=' + editor.value, function() {
+    httpRequest('client.php?chat-sample=' + valor, function() {
         var arreglo = JSON.parse(this.responseText);
         debug.value = arreglo
         boton.className = "btn btn-primary"
